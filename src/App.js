e43c96704b0318e1904a1cad69da7d8de11788b5
Rakhi Singh <rakhisingh030118@gmail.com>
2024-04-21 00:20:37 +0530

    Commit message for submodule changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from "react";
 import "./app.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import Popular from "./Components/Popular/Popular";
 import Offer from "./Components/Offer/Offer";
-import About from "./Components/About/About";             
+import About from "./Components/About/About";
 import Blog from "./Components/Blog/Blog";
 import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
-     <>
-     <Navbar/>
-     <Home/>
-     <Popular/>
-     <Offer/>         
-     <About/>
-     <Blog/>
-     <Footer/>
-     </>
+      <>
+        <Navbar />
+        <Home />
+        <Popular />
+        <Offer />
+        <About />
+        <Blog />
+        <Footer />
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
+export default App;
